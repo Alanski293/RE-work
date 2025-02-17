93bdff5e5c812c3fff67d94295da860affffe011
Alan <alanmaksimov293@gmail.com>
2025-02-17 15:03:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function ChangeColor() {
+    document.getElementById('container').style.color = "#1f1f1fd4"
+    document.getElementById('container').style.backgroundColor = "#a0c3ff"
+    document.getElementById('logo').style.color = "#0016d9d4"
+    document.getElementById('header').style.backgroundColor = "#fff"
+    document.getElementById('header').style.color = "#222"
+    document.getElementById('a').style.color = "#222"
+    document.getElementById('b').style.color = "#222"
+    document.getElementById('c').style.color = "#222"
+    document.getElementById('d').style.color = "#222"
+    document.getElementById('e').style.color = "#222"
+    document.getElementById('f').style.color = "#222"
+    document.getElementById('g').style.color = "#222"
+    document.getElementById('h').style.color = "#222"
+    document.getElementById('background').style.color = "#f0f8ffd4"
+    document.getElementById('ul').style.color = "#fff"
+}
+
+let input = document.querySelector('input');
+console.log(input.value);
+
+let button = document.querySelector('button');
+console.log(button.value);
